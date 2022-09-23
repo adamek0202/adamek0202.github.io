ec59014d95bd5092a7da6d893da2a511d699a36f
adamek0202 <adamek0584@gmail.com>
2022-09-23 21:20:07 +0200

    commit

--- js/mystery.js
+++ js/mystery.js
@@ -1,7 +1,16 @@
 function mystery() {
-    let checkbox = document.getElementById("mystery_checkbox");
-    if (checkbox.checked == true) {
-        window.location.href = "ahoj";
+    let checkbox1 = document.getElementById("mystery_checkbox1");
+    let checkbox2 = document.getElementById("mystery_checkbox2");
+    if (checkbox1.checked == false && checkbox2.checked == false) {
+        sleep(1000);
+        alert("Chyba");
     }
-    else window.location.href = "nazdar";
+
+
+
+    else { 
+        window.location.href = "nazdar";
+        sleep(1000);
+        checkbox1.checked = false;
+    }
 }

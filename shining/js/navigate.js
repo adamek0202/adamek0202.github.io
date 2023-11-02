d6639bdcd98a502b6853b5516e86174ec9fa3e28
Adam B <79209664+adamek0202@users.noreply.github.com>
2023-11-02 09:22:13 +0100

    Add files via upload

---
+++ shining/js/navigate.js
@@ -0,0 +1,11 @@
+function gotoimg1() {
+    window.location.href = "shining1.html";
+}
+
+function gotoimg2() {
+    window.location.href = "shining2.html";
+}
+
+function gotoimg3() {
+    window.location.href = "shining3.html";
+}
